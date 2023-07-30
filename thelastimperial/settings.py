28a78f45ce99e2a2cca061f81804aaedd6c87281
Ubuntu <ubuntu@ip-172-31-82-3.ec2.internal>
2023-07-30 23:41:05 +0000

    Fixed somes problems to run in production.

--- thelastimperial/settings.py
+++ thelastimperial/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e3!)!66cz0jm7&waw&2@k&=$uujk9m=a#sy3k21l&t@k_on2r('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", True)
+DEBUG = True if os.getenv("DJANGO_DEBUG", "True") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
